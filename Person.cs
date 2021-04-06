7a0e4838800875ef2c34783633c25dba0ed65d21
Alex <arriaga.91c@gmail.com>
2021-04-05 23:10:09 -0500

    Latest update

---
+++ Person.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project_4600___Medical_Care_Management_System
+{
+    public class Person
+    {
+        private string name_;
+        private int age_;
+        private double accessNumber_;
+
+    }
+}
