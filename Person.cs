e298c0c4fbff7ae8483659485e802c2aea57e5e2
Alex <arriaga.91c@gmail.com>
2021-04-05 23:21:03 -0500

    Update

--- Person.cs
+++ Person.cs
@@ -8,9 +8,40 @@ namespace Final_Project_4600___Medical_Care_Management_System
 {
     public class Person
     {
+        //#######################
+        //Data members
+        //#######################
         private string name_;
         private int age_;
         private double accessNumber_;
+
+        //#######################
+        //Constructors
+        //#######################
+        Person(string name, int age, double accessNumber) {
+            name_ = name;
+            age_ = age;
+            accessNumber_ = accessNumber;
+        }
+
+        //#######################
+        //Setters & Getters
+        //#######################
+        string GetName() {
+            return name_;
+        }
+
+        int GetAge() {
+            return age_;
+        }
+
+        double GetAcessNum() {
+            return accessNumber_;
+        }
+
+        //#######################
+        //Functions
+        //#######################
 
     }
 }
