0817a701ccc0875a7306098ddbae88cd34b38d8b
Eric Brewer <ebrewer4@my.apsu.edu>
2021-04-18 19:22:09 -0500

    Added Some functionality to create new doctor

--- Views/CreateDoctor.xaml.cs
+++ Views/CreateDoctor.xaml.cs
@@ -24,5 +24,34 @@ namespace Final_Project_4600___Medical_Care_Management_System.Views
         {
             InitializeComponent();
         }
+
+        private void new_doctor_Click(object sender, RoutedEventArgs e)
+        {
+            if(string.IsNullOrEmpty(newusername.Text))
+            {
+                MessageBox.Show("Please enter a username", "Message", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            if(string.IsNullOrEmpty(newpassword.Text))
+            {
+                MessageBox.Show("Please enter a password", "Message", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            if(string.IsNullOrEmpty(confirmpassword.Text) || confirmpassword.Text != newpassword.Text)
+            {
+                MessageBox.Show("Passwords do not match", "Message", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+            if(string.IsNullOrEmpty(firstname.Text) || string.IsNullOrEmpty(lastname.Text))
+            {
+                MessageBox.Show("Please enter first and last name", "Message", MessageBoxButton.OK, MessageBoxImage.Warning);
+                return;
+            }
+        }
+
+        private void back_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
     }
 }
