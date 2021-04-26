47dc13a2cb2e6ac2b733e6dd246f5a25906a32fd
Eric Brewer <ebrewer4@my.apsu.edu>
2021-04-25 22:27:50 -0500

    Added more database fucntionality

---
+++ dataConnection.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Final_Project_4600___Medical_Care_Management_System
+{
+    public static class dataConnection
+    {
+        public static SqlConnection Get_on_dbconnect()
+        {
+            
+        }
+    }
+}
